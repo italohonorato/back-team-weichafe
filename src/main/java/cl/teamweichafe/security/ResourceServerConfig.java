33b676876584d1d814da2d32712030dcb078d24c
Italo Honorato <italohonorato@gmail.com>
2021-06-18 22:10:58 -0400

    Cors configuration added

--- src/main/java/cl/teamweichafe/security/ResourceServerConfig.java
+++ src/main/java/cl/teamweichafe/security/ResourceServerConfig.java
@@ -1,9 +1,18 @@
 package cl.teamweichafe.security;
 
+import java.util.Arrays;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -17,7 +26,31 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		.antMatchers("/oauth/token", "/oauth/**").permitAll()
 		.antMatchers("/token/revoke").permitAll()
 		.antMatchers("/swagger-ui/**").permitAll()
-		.antMatchers("/api/**").authenticated();
+		.antMatchers("/api/**").authenticated()
+		.and()
+		.cors().configurationSource(corsConfigurationSource());
 	}
 
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(Arrays.asList("http://192.168.0.16:8080"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		config.setAllowCredentials(true);
+		config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		
+		return source;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		CorsFilter corsFilter = new CorsFilter(corsConfigurationSource());
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(corsFilter);
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		
+		return bean;
+	}
 }
